98f4bb1a7121b7ac420d35469956dbfd0fd9956a
Feihong Hsu <feihong.hsu@gmail.com>
2018-06-09 17:41:07 -0500

    Change js example to not automatically parse response content

--- axios_test.js
+++ axios_test.js
@@ -6,20 +6,26 @@ function getAccessToken() {
     return JSON.parse(text)['eventbrite_access_token']
 }
 
-let params = {
-    "token": getAccessToken(),
-    "location.address": "2100 S Wentworth Ave, Chicago, IL 60616",
-    "location.within": "1mi",
-    "sort_by": "date"
+let cfg = {
+    params: {
+        "token": getAccessToken(),
+        "location.address": "2100 S Wentworth Ave, Chicago, IL 60616",
+        "location.within": "1mi",
+        "sort_by": "date"
+    },
+    // transformResponse: [],
+    transformResponse: [data => data],
+    responseType: 'text',       // doesn't do anything
 }
-axios.get("https://www.eventbriteapi.com/v3/events/search/", {params})
+axios.get("https://www.eventbriteapi.com/v3/events/search/", cfg)
     .then(res => {
         console.log('Status:', res.status)
-        for (let evt of res.data.events) {
-            console.log(evt.name.text, evt.start.local.substring(0, 10))
-        }
-        let text = JSON.stringify(res.data, null, 2)
-        fs.writeFileSync('results.json', text)
+        fs.writeFileSync('results.json', res.data)
+        // for (let evt of res.data.events) {
+        //     console.log(evt.name.text, evt.start.local.substring(0, 10))
+        // }
+        // let text = JSON.stringify(res.data, null, 2)
+        // fs.writeFileSync('results.json', text)
     })
     .catch(err => {
         console.log(err)
