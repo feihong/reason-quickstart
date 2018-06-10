63301e70de688ed3ae3915d11723a97c75eb91d4
Feihong Hsu <feihong.hsu@gmail.com>
2018-06-09 21:53:55 -0500

    Add examples of using the js and j annotations to quoted string syntax

--- src/Hello.bs.js
+++ src/Hello.bs.js
@@ -6,6 +6,14 @@ var mesg = ( '你好世界！' );
 
 console.log(mesg);
 
+var mesg2 = "再见宇宙！";
+
+console.log(mesg2);
+
+var mesg3 = "Last message: " + (String(mesg2) + "");
+
+console.log(mesg3);
+
 console.log("\xe4\xbd\xa0\xe5\xa5\xbd\xe4\xb8\x96\xe7\x95\x8c\xef\xbc\x81");
 
 var john = {
@@ -17,5 +25,7 @@ var john = {
 console.log(john);
 
 exports.mesg = mesg;
+exports.mesg2 = mesg2;
+exports.mesg3 = mesg3;
 exports.john = john;
 /* mesg Not a pure module */
--- src/Hello.re
+++ src/Hello.re
@@ -1,12 +1,20 @@
 let mesg: string = [%bs.raw {| '你好世界！' |}];
 Js.log(mesg);
 
+let mesg2 = {js|再见宇宙！|js};
+Js.log(mesg2);
+
+/* Use string interpolation */
+let mesg3 = {j|Last message: $mesg2|j};
+Js.log(mesg3);
+
 /* This will be jibberish */
 Js.log("你好世界！");
 
 /* let thing = {"name": "blah"};
 Js.log(thing); */
 
+/* Old API for using JavaScript objects */
 type person = Js.t({
     .
     name: string,
